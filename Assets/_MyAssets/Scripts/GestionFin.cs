c8d10a6b2ae0a53dac4207db78ec2ab24f0a85bd
zack1200 <zakariaelbahodi@gmail.com>
2023-03-22 10:52:31 -0400

    err pointage et vas et viens perpetuelle

--- Assets/_MyAssets/Scripts/GestionFin.cs
+++ Assets/_MyAssets/Scripts/GestionFin.cs
@@ -7,10 +7,8 @@ public class GestionFin : MonoBehaviour
 {
     private GestionJeu _gestionJeu;
     private Player player;
-    float time;
-    float time2;
-    float time3;
     float tottime;
+
 
 
     private void Start()
@@ -21,47 +19,43 @@ public class GestionFin : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         int indexScene = SceneManager.GetActiveScene().buildIndex;
-        if (indexScene == 0)
+        if (collision.gameObject.tag == "Player")
         {
-            if (collision.gameObject.tag == "Player")
+            if (indexScene == 2)
             {
-            time =Time.time;
-            Debug.Log("Le temps du lv1 est de : " + time + "secondes");
-            }
-               
-        }
-        if (indexScene == 1)
-        {
-            if (collision.gameObject.tag == "Player")
-            {
-            time2 = Time.time;
-            Debug.Log("Le temps du lv2 est de : " + time2 + "secondes");
-            }
                 
-        }
-        if (indexScene == 2)
-        {
-            if (collision.gameObject.tag == "Player")
-            {
-                /* int erreurs = _gestionJeu.Pointage();
-                 gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
-                 Debug.Log("Le temps total est de : " + Time.time + "secondes");
-                 Debug.Log("vous avez accroche " + _gestionJeu.Pointage() + "obstacles");
-                 float total = Time.time + erreurs;
-                 Debug.Log("temps final : " + total);
-                 player.FinPartie();*/
-                time3 = Time.time;
-                tottime = time+time2+time3;
-                Debug.Log("temps lvl1 : " + time + "" + time2 + "" + time3 +"temps finale" + tottime);
-                
-                player.FinPartie();
-            }
-        }
-        else
-        {
-            //charger la nouvelle scene 
+                    /* int erreurs = _gestionJeu.Pointage();
+                     gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+                     Debug.Log("Le temps total est de : " + Time.time + "secondes");
+                     Debug.Log("vous avez accroche " + _gestionJeu.Pointage() + "obstacles");
+                     float total = Time.time + erreurs;
+                     Debug.Log("temps final : " + total);
+                     player.FinPartie();*/
+                    _gestionJeu.tps3(Time.time);
+                    tottime= Time.time;
+                    Debug.Log("temps lvl1 : " + _gestionJeu.temps1() + " temps lvl2 :" + _gestionJeu.temps2() + " temps lvl3 :" + _gestionJeu.temps3() + "temps finale" + tottime);
+                    Debug.Log("accrochages lvl1 : " + _gestionJeu.pt1() + " accrochages lvl2 :" + _gestionJeu.pt2() + " accrochages lvl3 :" + _gestionJeu.pt3() + "temps finale" + tottime);
 
-            SceneManager.LoadScene(indexScene + 1);
+                player.FinPartie();
+                
+            }
+            else
+            {
+                //charger la nouvelle scene 
+                if (indexScene == 0)
+                {
+                        _gestionJeu.tps(Time.time);
+                       
+
+                }
+                if (indexScene == 1)
+                {
+                        _gestionJeu.tps2(Time.time);
+
+
+                }
+                SceneManager.LoadScene(indexScene + 1);
+            }
         }
 
     }
--- Assets/_MyAssets/Scripts/GestionJeu.cs
+++ Assets/_MyAssets/Scripts/GestionJeu.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class GestionJeu : MonoBehaviour
 {
     private int _pointage;
+    float time;
+    float time2;
+    float time3;
+    int point1;
+    int point2;
+    int point3;
+    int totpts;
+    
+
     private void Awake()
     {
         int nbGestionJeu = FindObjectsOfType<GestionJeu>().Length;
         if (nbGestionJeu > 1)
         {
             Destroy(gameObject);
+        }
+        else 
+        {
+            DontDestroyOnLoad(gameObject);
         }
     }
     private void Start()
@@ -38,5 +51,60 @@ public class GestionJeu : MonoBehaviour
     public void Gameover()
     {
         Debug.Log("GameOver !" + Time.time);
+    }
+    public void tps(float tps)
+    {
+        time= tps;
+        
+    }
+    public void tps2(float tps)
+    {
+        time2 = tps-time;
+
+    }
+    public void tps3(float tps)
+    {
+        time3 = tps - time -time2;
+
+    }
+    public void pts1( )
+    {
+        point1 = _pointage;
+
+    }
+    public void pts2()
+    {
+        point2 = _pointage - point1;
+
+    }
+    public void pts3()
+    {
+        point3 = _pointage - point1-point2;
+
+    }
+
+    public float temps1() 
+    {
+        return time;
+    }
+    public float temps2()
+    {
+        return time2;
+    }
+    public float temps3()
+    {
+        return time3;
+    }
+    public float pt1()
+    {
+        return point1;
+    }
+    public float pt2()
+    {
+        return point2;
+    }
+    public float pt3()
+    {
+        return point3;
     }
 }
